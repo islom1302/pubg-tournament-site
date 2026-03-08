46f2117cd8697f133715710a6547efcce58ba847
islom1302 <aalita004@gmail.com>
2026-03-08 12:11:26 +1100

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const authRoutes = require("./routes/auth");
 const tournamentRoutes = require("./routes/tournament");
 const userRoutes = require("./routes/users");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+/* MongoDB connection */
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 app.use(cors());
 app.use(express.json());
@@ -20,5 +26,5 @@ app.use("/api/tournaments", tournamentRoutes);
 app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
